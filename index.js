0106ba9170153d7f5f1374b53a30155eb30e6fbc
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-09-07 16:13:59 -0600

    uso de mdlinks para analizar el archivo

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
-  // ...
-};
+const { mdLinks } = require('./mdLinks.js');
+mdLinks('./README.md', true)
+.then((result) => {
+    console.info(result);
+})
+.catch((error) => {
+    console.error(error)
+});
+
